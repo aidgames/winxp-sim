b58dbaabff62ffc4ba39666a0d79d52c9f2e92ec
aidgamesgg <ya.ivanov1302@yandex.ru>
2022-09-15 19:28:06 +0700

    start menu update :)

--- src/winui/startmenu_items.jsx
+++ src/winui/startmenu_items.jsx
@@ -3,6 +3,14 @@ export default function startmenu_items(){
         {
             "name": "Hello",
             "onclick": ()=>{alert(1)}
+        },
+        {
+            "name": "Hello2",
+            "onclick": ()=>{alert(2)}
+        },
+        {
+            "name": "Hello3",
+            "onclick": ()=>{alert(3)}
         }
     ]
 }
