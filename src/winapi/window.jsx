01060dc6ab0a0104af158885bb085c58b775cad8
aidgamesgg <ya.ivanov1302@yandex.ru>
2022-09-16 12:54:32 +0700

    .

--- src/winapi/window.jsx
+++ src/winapi/window.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 
 import { useSpring, animated } from "@react-spring/web";
 import { useDrag } from "@use-gesture/react";
+import { Resizable } from "react-resizable";
 
 export default function Window({children}, ...props){
     const [{ x, y }, api] = useSpring(() => ({ x: 5, y: 5}))
@@ -12,25 +13,36 @@ export default function Window({children}, ...props){
     })
     })
     const [win, setWin]=useState(true);
+    const [width, setWidth]=useState(265);
+    const [height, setHeight]=useState(2213);
+    
     if (win){
         return (
-        <animated.div className="winui__window" style={{
-            "marginLeft": x,
-            "marginTop": y
-        }}>
-            <div className="winui__window__frame" {...bind()}>
-                <div className="winui__window__frame__buttons">
-                    <span className="winui__window__frame__buttons__min">-</span>
-                    <span className="winui__window__frame__buttons__max">[]</span>
-                    <span className="winui__window__frame__buttons__close" onClick={()=>{
-                        setWin(false)
-                    }}>X</span>
+        <Resizable height={height} width={width} onResize={(e, {element, size, handle})=>{
+            setWidth(size.width)
+            setHeight(size.height)
+            console.log(size)
+        }} handle={<div>Resize</div>}>
+            <animated.div className="winui__window" style={{
+                "marginLeft": x,
+                "marginTop": y
+            }}>
+           
+                <div className="winui__window__frame" {...bind()}>
+                    <div className="winui__window__frame__buttons">
+                        <span className="winui__window__frame__buttons__min">-</span>
+                        <span className="winui__window__frame__buttons__max">[]</span>
+                        <span className="winui__window__frame__buttons__close" onClick={()=>{
+                            setWin(false)
+                        }}>X</span>
+                    </div>
                 </div>
-            </div>
-            <div className="winui__window__content">
-                {children}
-            </div>  
-        </animated.div>
+                <div className="winui__window__content">
+                    {children}
+                </div>  
+            </animated.div>
+        </Resizable>
+        
         )
     }
 }
